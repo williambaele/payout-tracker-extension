6517c9f7a876860b09cedf96ef21d813b7093e4c
William Baele <baelewilliam1999@gmail.com>
2023-03-22 14:01:34 +0100

    Added auto clear input value after sumbit

--- app.js
+++ app.js
@@ -1,21 +1,27 @@
-document.getElementById("waiting").addEventListener("click", loadWaitingPayout);
-function loadWaitingPayout(){
-  document.querySelector("#main").innerHTML = `
-  <div class="flex bg-gray-300 rounded-md px-4 gap-4 py-2 items-center">
-    <div>
-      <h2 class="font-medium text-md rounded-full bg-[#0f7173] p-1 flex items-center justify-center text-white">1</h2>
+document.getElementById("waiting").addEventListener("click", displayWaitingPayout);
+function displayWaitingPayout() {
+  const saleList = JSON.parse(localStorage.getItem("saleData")) || [];
+  document.querySelector("#waiting").classList.remove('bg-[#0f7173]');
+  document.querySelector("#waiting").classList.add('bg-[#6faaab]');
+  document.querySelector("#main").innerHTML = saleList.map(sale => `
+    <div class="flex bg-gray-300 rounded-md px-4 gap-4 py-2 items-center">
+      <div class="flex justify-between items-center w-full">
+        <div>
+          <p class="text-lg text-white">${sale}</p>
+        </div>
+        <div class="flex gap-2">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#0f7173" class="cursor-pointer bi bi-check-circle-fill" viewBox="0 0 16 16">
+            <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/>
+          </svg>
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#FF0000" class="cursor-pointer bi bi-x-circle-fill" viewBox="0 0 16 16">
+            <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z"/>
+          </svg>
+        </div>
+      </div>
     </div>
-    <div class="flex justify-between items-center w-full">
-      <p class="text-lg text-white">ID: 253784</p>
-      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#0f7173" class="bi bi-check-circle-fill" viewBox="0 0 16 16">
-        <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/>
-      </svg>
-    </div>
-  </div>
-  `;
+  `).join('');
 }
-
-loadWaitingPayout();
+displayWaitingPayout();
 
 
 /* SALE'S SAVING FUNCTION*/
