e008bc721119d1e9834add2032b1c84412fba0ad
William Baele <baelewilliam1999@gmail.com>
2023-03-22 15:51:28 +0100

    Added auto clear input value after sumbit

--- app.js
+++ app.js
@@ -13,9 +13,11 @@ function displayWaitingPayout() {
           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#0f7173" class="cursor-pointer bi bi-check-circle-fill" viewBox="0 0 16 16">
             <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/>
           </svg>
-          <svg xmlns="http://www.w3.org/2000/svg" id="deleteitem" width="16" height="16" fill="#FF0000" class="cursor-pointer bi bi-x-circle-fill" viewBox="0 0 16 16">
-            <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z"/>
-          </svg>
+          <div id="deleteitem">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#FF0000" class="cursor-pointer bi bi-x-circle-fill" viewBox="0 0 16 16">
+              <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z"/>
+            </svg>
+          </div>
         </div>
       </div>
     </div>
@@ -63,11 +65,12 @@ function saveSale() {
 }
 
 /* SALE'S DELETE FUNCTION*/
-document.querySelector("#deleteitem").addEventListener("click", deleteSale());
+document.querySelector("#deleteitem").addEventListener("click", deleteSale);
 
-function deleteSale(event) {
+function deleteSale() {
+  console.log("hi");
   const saleList = JSON.parse(localStorage.getItem("saleData")) || [];
-  const indexToDelete = event.target.closest('[data-index]').getAttribute('data-index');
+  const indexToDelete = parseInt(getAttribute('data-index'));
   saleList.splice(indexToDelete, 1);
   localStorage.setItem("saleData", JSON.stringify(saleList));
   displayWaitingPayout();
